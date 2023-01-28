323b1427ebc8ba803c4bb0bead6db7e16265e9c4
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-01-28 23:41:31 +0100

    feat(pages): add metadata to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { Navbar } from '@/components';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>NextFire</title>
+        <meta name="description" content="A Dev.to clone." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,26 @@
-import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>NextFire</title>
-        <meta name="description" content="A Dev.to clone." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>NextFire</h1>
-        <ul>
-          <li>
-            <Link href="/login">Log in</Link>
-          </li>
-          <li>
-            <Link href="/batman">Go to batman user</Link>
-          </li>
-          <li>
-            <Link href="/batman/some-post">Go to batman post</Link>
-          </li>
-          <li>
-            <Link href="/admin">Go to admin</Link>
-          </li>
-          <li>
-            <Link href="/admin/some-post">Go to admin post edit</Link>
-          </li>
-        </ul>
-      </main>
-    </>
+    <main>
+      <h1>NextFire</h1>
+      <ul>
+        <li>
+          <Link href="/login">Log in</Link>
+        </li>
+        <li>
+          <Link href="/batman">Go to batman user</Link>
+        </li>
+        <li>
+          <Link href="/batman/some-post">Go to batman post</Link>
+        </li>
+        <li>
+          <Link href="/admin">Go to admin</Link>
+        </li>
+        <li>
+          <Link href="/admin/some-post">Go to admin post edit</Link>
+        </li>
+      </ul>
+    </main>
   );
 }
