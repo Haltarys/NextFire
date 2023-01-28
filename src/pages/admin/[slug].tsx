4ffbc2e2804a6d1cb2420dbbc45c0decf9ffbfda
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-01-28 17:08:54 +0100

    feat(app): add all route pages

---
+++ src/pages/admin/[slug].tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function AdminPostEditPage() {
+  return (
+    <div>
+      <h1>Admin post edit for slug</h1>
+      <Link href="/">Back</Link>
+    </div>
+  );
+}
