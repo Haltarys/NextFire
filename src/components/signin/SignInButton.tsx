bfcfd82de5874a108ca20afd4f3846952f576c70
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-04-14 20:24:32 +0200

    fix(signin): redirect only if username is set

--- src/components/signin/SignInButton.tsx
+++ src/components/signin/SignInButton.tsx
@@ -1,15 +1,18 @@
 import { auth, googleAuthProvider } from '@/lib/firebase/firebase';
+import { UserDataContext } from '@/lib/hooks/userData';
 import { signInWithPopup } from 'firebase/auth';
 import { useRouter } from 'next/router';
+import { useContext } from 'react';
 import { toast } from 'react-hot-toast';
 
 export default function SignInButton() {
   const router = useRouter();
+  const { username } = useContext(UserDataContext);
 
   const signInWithGoogle = async () => {
     await signInWithPopup(auth, googleAuthProvider)
       .then(() => {
-        router.push('/admin');
+        username && router.push('/admin');
         toast.success('Signed in with Google!');
       })
       .catch(() => {
