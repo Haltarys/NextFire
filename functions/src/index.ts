667e27fca5f3cb028da281c06aa3e37776799fa9
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-04-16 17:55:15 +0200

    feat(firebase): setup emulator

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,13 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+// import { initializeApp } from 'firebase-admin';
 
-admin.initializeApp();
+// const app = initializeApp();
+
+if (process.env.NODE_ENV !== 'production') {
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:9099';
+  process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+  process.env.FIREBASE_STORAGE_EMULATOR_HOST = 'localhost:9199';
+}
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -10,3 +16,11 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
   response.send('Hello from Firebase!');
 });
+
+// export const deleteImagesTiedToPost = functions.firestore
+//   .document('users/{userId}/posts/{postId}')
+//   .onDelete(async (snap, context) => {
+//     const { postId } = context.params;
+
+//     const folderPath = `uploads/${context.auth?.uid}/${postId}`;
+//   });
