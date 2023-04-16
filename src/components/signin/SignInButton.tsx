6c535d9235daa28d23e67afbdc9a2b1dfe12d309
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-04-16 21:07:48 +0200

    fix(signin): redirect to admin if username is set

--- src/components/signin/SignInButton.tsx
+++ src/components/signin/SignInButton.tsx
@@ -1,18 +1,11 @@
 import { auth, googleAuthProvider } from '@/lib/firebase/firebase';
-import { UserDataContext } from '@/lib/hooks/userData';
 import { signInWithPopup } from 'firebase/auth';
-import { useRouter } from 'next/router';
-import { useContext } from 'react';
 import { toast } from 'react-hot-toast';
 
 export default function SignInButton() {
-  const router = useRouter();
-  const { username } = useContext(UserDataContext);
-
   const signInWithGoogle = async () => {
     await signInWithPopup(auth, googleAuthProvider)
       .then(() => {
-        username && router.push('/admin');
         toast.success('Signed in with Google!');
       })
       .catch(() => {
