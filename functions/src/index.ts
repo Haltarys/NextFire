53660f434e4fc715fc4063aca5001a1d66e1e58f
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-04-20 21:04:21 +0200

    feat(post): add functions to delete hearts upon post deletion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import * as functions from 'firebase-functions';
-// import { initializeApp } from 'firebase-admin';
-
-// const app = initializeApp();
+import { initializeApp } from 'firebase-admin/app';
+import { storage, firestore } from 'firebase-admin';
 
 if (process.env.NODE_ENV !== 'production') {
   process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:9099';
@@ -9,18 +8,51 @@ if (process.env.NODE_ENV !== 'production') {
   process.env.FIREBASE_STORAGE_EMULATOR_HOST = 'localhost:9199';
 }
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+// When deleting a document, Firestore does not delete all of its subcollections,
+// so we need to do it manually.
+export const deleteHeartsTiedToPost = functions.firestore
+  .document('users/{userId}/posts/{postId}')
+  .onDelete(async (snapshot, context) => {
+    const { postId } = context.params;
+    const batch = firestore().batch();
 
-// export const deleteImagesTiedToPost = functions.firestore
-//   .document('users/{userId}/posts/{postId}')
-//   .onDelete(async (snap, context) => {
-//     const { postId } = context.params;
+    return snapshot.ref
+      .collection('hearts')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          batch.delete(doc.ref);
+        });
+      })
+      .then(() => {
+        batch.commit();
+      })
+      .then(() => {
+        functions.logger.info(`Deleted hearts for post ${postId}.`);
+      })
+      .catch((err) => {
+        functions.logger.error(
+          `Error deleting hearts for post ${postId}: ${err}`,
+        );
+      });
+  });
 
-//     const folderPath = `uploads/${context.auth?.uid}/${postId}`;
-//   });
+export const deleteImagesTiedToPost = functions.firestore
+  .document('users/{userId}/posts/{postId}')
+  .onDelete(async (snapshot, context) => {
+    const { userId, postId } = context.params;
+
+    return storage()
+      .bucket()
+      .deleteFiles({ prefix: `uploads/${userId}/posts/${postId}` })
+      .then(() => {
+        functions.logger.info(`Deleted images for post ${postId}.`);
+      })
+      .catch((err) => {
+        functions.logger.error(
+          `Error deleting images for post ${postId}: ${err}`,
+        );
+      });
+  });
